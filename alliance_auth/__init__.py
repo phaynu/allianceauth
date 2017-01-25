d1f4e7eff0cf5b5cfe897bc157a7690ecbdd945f
Adarnof <adarnof@gmail.com>
2017-01-25 18:01:22 -0500

    Check current version on startup. Warn if on development branch. No need to manually bump version number anymore.

--- alliance_auth/__init__.py
+++ alliance_auth/__init__.py
@@ -4,5 +4,15 @@ from __future__ import absolute_import, unicode_literals
 # Django starts so that shared_task will use this app.
 from .celeryapp import app as celery_app  # noqa
 
-__version__ = '1.14.2'
-NAME = 'Alliance Auth v%s' % __version__
+import subprocess
+try:
+    __version__ = subprocess.Popen(['git', 'status'], stdout=subprocess.PIPE).communicate()[0].decode('utf-8').split('\n')[0].split()[-1]
+except (KeyError, AttributeError):
+    __version__ = 'master'
+
+if __version__ == 'master':
+    print('\033[1;31mOn development branch. Not guaranteed to work. Data loss may occur.\033[0m')
+    __version__ = 'dev'
+
+NAME = 'Alliance Auth %s' % __version__
+
